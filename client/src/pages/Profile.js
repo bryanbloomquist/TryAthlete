30bb6f4223f5b06f7483fb83a6a6d3f51799c8fc
ScottTAnderson <42193132+ScottTAnderson@users.noreply.github.com>
2019-05-07 16:07:32 -0500

    record a workout template

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,14 +4,19 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import API from "../utils/API";
-import RunCard from "../components/ActivityCards/RunCard/RunCard.js";
-import BikeCard from "../components/ActivityCards/BikeCard/BikeCard.js";
-import SwimCard from "../components/ActivityCards/SwimCard/SwimCard.js";
+import ActivityCard from "../components/ActivityCard/ActivityCard.js";
+import Run from "../components/images/run.png"
+import Bike from "../components/images/bike.png"
+import Swim from "../components/images/swim.png"
+// import run from "../components/images"
 
 class Profile extends Component {
     state = {
         user: {},
-        activity: ["run", "swim", "bike"]
+        // activity: ["Run", "Bike Ride", "Swim"],
+        // rbunits: ["mi", "km"],
+        // sunits: ["meter", "yard"],
+        // color: ["#ed4b4b", "#4fc147", "#4b68ed"]
     };
     // When this component mounts, grab the user with the _id of this.props.match.params.id
     componentDidMount() {
@@ -39,10 +44,24 @@ class Profile extends Component {
                     <h1 className="text-center">User Profile Area</h1>
                 </Row>
                 <Row>
-                    <RunCard>{this.state.activity[0]}</RunCard>
-                    try setstate
-                    <BikeCard></BikeCard>
-                    <SwimCard></SwimCard>
+                    <ActivityCard
+                        activity={"Run"}
+                        units={["mi", "km"]}
+                        color={"#ed4b4b"}
+                        img={Run}
+                    />
+                    <ActivityCard
+                        activity={"Ride"}
+                        units={["mi", "km"]}
+                        color={"#4fc147"}
+                        img={Bike}
+                    />
+                    <ActivityCard
+                        activity={"Swim"}
+                        units={["meters", "yards"]}
+                        color={"#4b68ed"}
+                        img={Swim}
+                    />
                 </Row>
             </Container >
         );
