c76fa9c6194fe4dd79c9118bf6cd08d80ddf839a
Steve Thompson <sthmpsn@gmail.com>
2019-05-08 21:35:55 -0500

    added badges controller

---
+++ controllers/badges.js
@@ -0,0 +1,17 @@
+const db = require("../models");
+
+// Defining methods for the UsersController
+module.exports = {
+  findAll: function(req, res) {
+    db.User
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.User
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
