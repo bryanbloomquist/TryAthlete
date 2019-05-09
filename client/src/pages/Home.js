a6968f7aeb9377bdc4888b6633b49ee5be126176
bloomy215 <bmbloomquist@gmail.com>
2019-05-08 23:27:18 -0500

    google login is functional

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,53 +1,33 @@
 import React, { Component } from "react";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin } from "react-google-login";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import API from "../utils/API";
-import config from "../config.json";
 
 class Details extends Component {
-    constructor() {
-        super();
-        this.state = { 
-            isAuthenticated: false, 
-            user: null, 
-            token: "" };
-    }
-    
     state = {
         user: {}
     };
 
-    logout = () => {
-        this.setState({
-            isAuthenticated: false,
-            token: "",
-            user: null
-        })
-    };
+    responseGoogleSuccess = ( response ) => {
+        console.log( response );
+        let user = {
+            "email": response.profileObj.email,
+            "familyName": response.profileObj.familyName,
+            "givenName": response.profileObj.givenName,
+            "googleId": response.googleId,
+            "imageUrl": response.profileObj.imageUrl,
+            "name": response.profileObj.name
+        }
+        console.log( user );
+        window.location.assign( "/user/:id/dashboard")
+    }
 
-    googleResponse = ( response ) => {
-        const tokenBlob = new Blob([ JSON.stringify({ access_token: response.accessToken }, null, 2 )], { type : "application/json"});
-        const options = {
-            method: "POST",
-            body: tokenBlob,
-            mode: "cors",
-            cache: "default"
-        };
-        fetch( "http://localhost:3000/api/v1/auth/google", options ).then(( r ) => {
-            const token = r.headers.get( "x-auth-token" );
-            r.json().then(( user ) => {
-                if ( token ) {
-                    this.setState({ isAuthenticated: true, user, token })
-                }
-            });
-        })
-        console.log ( response );
-    };
-
-    onFailure = ( error ) => { alert( error )};
+    responseGoogleFailure = ( response ) => {
+        console.log( response );
+    }
 
     // When this component mounts, grab the user with the _id of this.props.match.params.id
     componentDidMount() {
@@ -61,32 +41,6 @@ class Details extends Component {
     }
 
     render() {
-
-        let content = !!this.state.isAuthenticated ? 
-            (
-                <div>
-                    <p>Authenticated</p>
-                    <div>
-                        { this.state.user.email }
-                    </div>
-                    <div>
-                        <button 
-                            onClick = { this.logout }
-                            className = "button"
-                        >
-                            Logout
-                        </button>
-                    </div>
-                </div>
-            ) : (
-                <GoogleLogin
-                    clientId = "907322878909-ceh0tltstqr7ht4eidho9ehj73bs7t1p.apps.googleusercontent.com"
-                    buttonText = "Google Login"
-                    onSuccess = { this.googleResponse }
-                    onFailure = { this.onFailure }
-                />
-            );
-
         return (
             <Container fluid>
                 <Row>
@@ -103,7 +57,13 @@ class Details extends Component {
                     <Col>
                         <h2 className="text-center">Home Page Info Here</h2>
                         {/* <button className="btn btn-success" onClick={() => this.logIn()}>Login</button> */}
-                        { content }
+                        <GoogleLogin
+                            clientId = "907322878909-ceh0tltstqr7ht4eidho9ehj73bs7t1p.apps.googleusercontent.com"
+                            buttonText = "Login"
+                            onSuccess = { this.responseGoogleSuccess }
+                            onFailure = { this.responseGoogleFailure }
+                            cookiePolicy = { "single_host_origin" }
+                        />
                     </Col>
                 </Row>
             </Container>
