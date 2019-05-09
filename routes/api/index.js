e618ffcf7ffafaf51572d218eac7047285dd76df
Steve Thompson <sthmpsn@gmail.com>
2019-05-08 21:18:52 -0500

    updated user api routes and added badge routes with John

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const badgeRoutes = require("./badges");
 
 // User routes
 router.use("/users", userRoutes);
+router.use("/badges", badgeRoutes);
 
 module.exports = router;
