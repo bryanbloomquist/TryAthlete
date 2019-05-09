25de9d7c7b940e14896b71562d3ddb081a5c0183
Steve Thompson <sthmpsn@gmail.com>
2019-05-08 21:43:02 -0500

    renamed badges controller

--- controllers/badgesController.js
+++ controllers/badgesController.js
@@ -1,15 +1,15 @@
 const db = require("../models");
 
-// Defining methods for the UsersController
+// Defining methods for the BadgesController
 module.exports = {
   findAll: function(req, res) {
-    db.User
+    db.Badges
       .find(req.query)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User
+    db.Badges
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
