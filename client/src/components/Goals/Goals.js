dc7919d1c9c9b815312d581223c7018d477cc379
Steve Thompson <sthmpsn@gmail.com>
2019-05-12 14:21:27 -0500

    static form created.  Will be trying to pull in user data next

--- client/src/components/Goals/Goals.js
+++ client/src/components/Goals/Goals.js
@@ -3,6 +3,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from 'react-bootstrap/Form'
+import Button from "react-bootstrap/Button";
 
 var localProps = {
     user: {
@@ -15,42 +16,70 @@ var localProps = {
 function Goals(props) {
     console.log(props);
     return (
-        <Container fluid className="py-0">
+        <Container fluid className="pb-5">
             <Row>
                 <Col className="display-3 text-center py-5">
                     Goal Details for {localProps.user.givenName}
                 </Col>
             </Row>
             <Row className="text-center">
-                <Col>
+                <Col md={4}>
                     <Form>
-                        <Form.Group controlId="goalActivity">
-                            <Form.Label>Activity</Form.Label>
-                            <Form.Control as="select">
-                                <option>Run</option>
-                                <option>Bike</option>
-                                <option>Swim</option>
-                            </Form.Control>
+                        <Form.Group as={Row} controlId="goalActivity">
+                            <Form.Label column sm={4}>Activity:</Form.Label>
+                            <Col sm={6}>
+                                <Form.Control as="select">
+                                    <option>Run</option>
+                                    <option>Bike</option>
+                                    <option>Swim</option>
+                                </Form.Control>
+                            </Col>
                         </Form.Group>
-                        <Form.Group controlId="goalType">
-                            <Form.Label>Type</Form.Label>
-                            <Form.Control as="select">
+                        <Form.Group as={Row} controlId="goalType">
+                            <Form.Label column sm={4}>Type:</Form.Label>
+                            <Col sm={6}>
+                                <Form.Control as="select">
                                 <option>Distance</option>
                                 <option>Frequency</option>
                                 <option>Time</option>
                             </Form.Control>
+                            </Col>
                         </Form.Group>
-                        <Form.Group controlId="goalQty">
-                            <Form.Label>Qty</Form.Label>
-                            <Form.Control type="text"/>
+                        <Form.Group as={Row} controlId="goalQty">
+                            <Form.Label column sm={4}>Qty:</Form.Label>
+                            <Col sm={6}>
+                                <Form.Control type="text" />
+                            </Col>
                         </Form.Group>
-
-
-                        
+                        <Form.Group as={Row} controlId="goalUnit">
+                            <Form.Label column sm={4}>Unit:</Form.Label>
+                            <Col sm={6}>
+                                <Form.Control as="select">
+                                    <option>Miles</option>
+                                    <option>Kilometer</option>
+                                    <option>Hours</option>
+                                    <option>Days</option>
+                                </Form.Control>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} controlId="goalTimeframe">
+                            <Form.Label column sm={4}>Time frame:</Form.Label>
+                            <Col sm={6}>
+                                <Form.Control as="select">
+                                    <option>Today</option>
+                                    <option>This Week</option>
+                                </Form.Control>
+                            </Col>
+                        </Form.Group>
+                        <Col sm={{ span: 9, offset: 1}}>
+                            <Button variant="warning" size="lg" block >
+                                Create New Goal
+                            </Button>
+                        </Col>
                     </Form>
 
                 </Col>
-                <Col>
+                <Col md={8}>
                     On Deck <br />
                     Lifetime Acheivements
                 </Col>
