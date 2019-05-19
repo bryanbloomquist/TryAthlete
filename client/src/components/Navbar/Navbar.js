0b5ae46541b1514b64dbb46c810f934c8711d203
bloomy215 <bmbloomquist@gmail.com>
2019-05-19 14:30:08 -0500

    adjusted navbar, dashboard now displays how many badges user has earned

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
-// import Link from "react-router-dom/Link";
 import Logo from "../images/logo.svg";
 const Link = require("react-router-dom").Link;
-// import Home from "../images/home2.png";
-// import Login from "../images/login2.png"
 
 const style = {
     display: "block",
@@ -26,22 +22,20 @@ function NavbarArea(props) {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <NavDropdown title="Menu">
-                        <Link style={style} to="/dashboard">Dashboard</Link>
-                        <Link style={style} to="/goals">Goals</Link>
-                        <Link style={style} to="/challenges">Challenges</Link>
-                        <Link style={style} to="/badges">Badges</Link>
-                        <Link style={style} to="/social">Social</Link>
-                    </NavDropdown>
+                    <Link style={style} to="/dashboard">Dashboard</Link>
+                    <Link style={style} to="/goals">Goals</Link>
+                    <Link style={style} to="/challenges">Challenges</Link>
+                    <Link style={style} to="/badges">Badges</Link>
+                    <Link style={style} to="/social">Social</Link>
                 </Nav>
                 <Form inline className="searchBar">
                     <FormControl type="text" placeholder="Search for friends" className="mr-sm-2" />
                     <Button className="searchButton" variant="outline-success">Search</Button>
                 </Form>
-                <Link to="/profile"><Navbar.Brand>
-                    <img className="avatar" src={props.children.imageUrl} alt={props.children.givenName}></img>
-                </Navbar.Brand></Link>
             </Navbar.Collapse>
+            <Link to="/profile"><Navbar.Brand>
+                <img className="avatar" src={props.children.imageUrl} alt={props.children.givenName}></img>
+            </Navbar.Brand></Link>
         </Navbar >
     );
 }
