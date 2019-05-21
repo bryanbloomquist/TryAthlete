86f8ab186904903438b1796a1ee227b7405f79e0
bloomy215 <bmbloomquist@gmail.com>
2019-05-20 19:39:40 -0500

    dashboard responsiveness corrected

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -25,59 +25,71 @@ function Dashboard(props) {
                 </Col>
             </Row>
             <Row>
-                <ActivityCard
-                    sport={"Run"}
-                    units={["mi", "km", "minutes"]}
-                    color={"#ed4b4b"}
-                    img={Run}
-                    user={props}
-                    activity={props.runActivity}
-                    onLogClick={props.onLogClick}
-                    onDistanceChange={props.onDistanceChange}
-                    onDurationChange={props.onDurationChange}
-                    onUnitChange={props.onUnitChange}
-                    handleClose={props.handleClose}
-                    handleShow={props.handleShow}
-                />
-                <ActivityCard
-                    sport={"Ride"}
-                    units={["mi", "km", "minutes"]}
-                    color={"#4fc147"}
-                    img={Bike}
-                    user={props}
-                    activity={props.rideActivity}
-                    onLogClick={props.onLogClick}
-                    onDistanceChange={props.onDistanceChange}
-                    onDurationChange={props.onDurationChange}
-                    onUnitChange={props.onUnitChange}
-                    handleClose={props.handleClose}
-                    handleShow={props.handleShow}
-                />
-                <ActivityCard
-                    sport={"Swim"}
-                    units={["meters", "yards", "minutes"]}
-                    color={"#4b68ed"}
-                    img={Swim}
-                    user={props}
-                    activity={props.swimActivity}
-                    onLogClick={props.onLogClick}
-                    onDistanceChange={props.onDistanceChange}
-                    onDurationChange={props.onDurationChange}
-                    onUnitChange={props.onUnitChange}
-                    handleClose={props.handleClose}
-                    handleShow={props.handleShow}
-                />
-            </Row>
-            <Row>
-                <GoalsCard user={props.user} />
-                <ChallengesCard user={props.user} />
-                <BadgesCard user={props.user} />
-            </Row>
-            <Row>
-                <SocialCard 
-                user={props.user}
-                friends={props.friends}
-                 />
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <ActivityCard
+                        sport={"Run"}
+                        units={["mi", "km", "minutes"]}
+                        color={"#ed4b4b"}
+                        img={Run}
+                        user={props}
+                        activity={props.runActivity}
+                        onLogClick={props.onLogClick}
+                        onDistanceChange={props.onDistanceChange}
+                        onDurationChange={props.onDurationChange}
+                        onUnitChange={props.onUnitChange}
+                        handleClose={props.handleClose}
+                        handleShow={props.handleShow}
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <ActivityCard
+                        sport={"Ride"}
+                        units={["mi", "km", "minutes"]}
+                        color={"#4fc147"}
+                        img={Bike}
+                        user={props}
+                        activity={props.rideActivity}
+                        onLogClick={props.onLogClick}
+                        onDistanceChange={props.onDistanceChange}
+                        onDurationChange={props.onDurationChange}
+                        onUnitChange={props.onUnitChange}
+                        handleClose={props.handleClose}
+                        handleShow={props.handleShow}
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <ActivityCard
+                        sport={"Swim"}
+                        units={["meters", "yards", "minutes"]}
+                        color={"#4b68ed"}
+                        img={Swim}
+                        user={props}
+                        activity={props.swimActivity}
+                        onLogClick={props.onLogClick}
+                        onDistanceChange={props.onDistanceChange}
+                        onDurationChange={props.onDurationChange}
+                        onUnitChange={props.onUnitChange}
+                        handleClose={props.handleClose}
+                        handleShow={props.handleShow}
+                    />
+                </Col>
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <GoalsCard user={props.user} />
+                </Col>
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <ChallengesCard user={props.user} />
+                </Col>
+                <Col xs={12} sm={6} lg={4} className="mb-4">
+                    <BadgesCard user={props.user} />
+                </Col>
+            {/* </Row>
+            <Row> */}
+                <Col xs={12}>
+                    <SocialCard 
+                        user={props.user}
+                        friends={props.friends}
+                    />                    
+                </Col>
             </Row>
         </Container >
 
