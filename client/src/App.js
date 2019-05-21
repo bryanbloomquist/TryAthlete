60d228b61d2523694c20da91d15a00d8c786879f
Scott Anderson <Scott.T.Anderson50@gmail.com>
2019-05-20 19:16:01 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -370,31 +370,31 @@ class App extends Component {
         }
         if (value.goalType === "Time" && value.gaolQty <= runDuration && value.isAchieved === false) {
           alert("You achieved a running time goal!")
-          API.updateGoal(true, this.state.user._id)
+          API.updateGoal(true, this.state.user._id, value.id)
             .then(res => this.setState({ user: res.data }));
         }
       }
       if (value.sport === "Ride") {
         if (value.goalType === "Distance" && value.goalQty <= rideDistance && value.isAchieved === false) {
           alert("You achieved a biking distance goal!")
-          API.updateGoal(true, this.state.user._id)
+          API.updateGoal(true, this.state.user._id, value.id)
             .then(res => this.setState({ user: res.data }));
         }
         if (value.goalType === "Time" && value.gaolQty <= rideDuration && value.isAchieved === false) {
           alert("You achieved a biking time goal!")
-          API.updateGoal(true, this.state.user._id)
+          API.updateGoal(true, this.state.user._id, value.id)
             .then(res => this.setState({ user: res.data }));
         }
       }
       if (value.sport === "Swim") {
         if (value.goalType === "Distance" && value.goalQty <= swimDistance && value.isAchieved === false) {
           alert("You achieved a swimming distance goal!")
-          API.updateGoal(true, this.state.user._id)
+          API.updateGoal(true, this.state.user._id, value.id)
             .then(res => this.setState({ user: res.data }));
         }
         if (value.goalType === "Time" && value.gaolQty <= swimDuration && value.isAchieved === false) {
           alert("You achieved a swimming time goal!")
-          API.updateGoal(true, this.state.user._id)
+          API.updateGoal(true, this.state.user._id, value.id)
             .then(res => this.setState({ user: res.data }));
         }
       }
